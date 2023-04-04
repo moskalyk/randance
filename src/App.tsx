2f82027196a5d6b5482a50195b7760e9b4ca7ce7
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-04 16:22:48 -0400

    added native api

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,29 @@ import './App.css';
 import useGyroscope from 'react-hook-gyroscope'
 
 const ComponentWithGyroscope = () => {
-  const gyroscope = useGyroscope()
-  
-  return !gyroscope.error ? (
+  const [alpha, setAlpha] = React.useState(0)
+  const [beta, setBeta] = React.useState(0)
+  const [gamma, setGamma] = React.useState(0)
+
+  // const gyroscope = useGyroscope()
+  window.addEventListener('deviceorientation', handleOrientation);
+
+  function handleOrientation(event: any) {
+    const alpha0 = event.alpha;
+    const beta0 = event.beta;
+    const gamma0 = event.gamma;
+    setBeta(alpha0)
+    setBeta(beta0)
+    setGamma(gamma0)
+    // Do stuff...
+  }
+
+  return (
     <ul>
-      <li className='prompt'>X: {gyroscope.x}</li>
-      <li className='prompt'>Y: {gyroscope.y}</li>
-      <li className='prompt'>Z: {gyroscope.z}</li>
+      <li className='prompt'>X: {alpha}</li>
+      <li className='prompt'>Y: {beta}</li>
+      <li className='prompt'>Z: {gamma}</li>
     </ul>
-  ) : (
-    <p className='prompt'>No gyroscope, sorry.</p>
   )
 }
 
