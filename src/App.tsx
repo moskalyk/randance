e7ae39b0d0a2696fb8125dfb9a78c10deb55074e
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-05 09:13:53 -0400

    added rotation detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
   requestPermission?: () => Promise<'granted' | 'denied'>;
 }
 
+function detectGreaterAngle(x: number, y: number, z: number) {
+  // Convert the x, y, and z values to radians
+  const radX = Math.atan2(x, Math.sqrt(y*y + z*z));
+  const radY = Math.atan2(y, Math.sqrt(x*x + z*z));
+  const radZ = Math.atan2(Math.sqrt(x*x + y*y), z);
+  
+  // Convert the radians to degrees
+  const degX = radX * (180/Math.PI);
+  const degY = radY * (180/Math.PI);
+  const degZ = radZ * (180/Math.PI);
+  
+  // Check if any of the angles are greater than 50 degrees
+  if (Math.abs(degX) > 50 || Math.abs(degY) > 50 || Math.abs(degZ) > 50) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getPercentageTrueFalse(arr: any) {
+  const countTrue = arr.reduce((acc: any, val: any) => val ? acc + 1 : acc, 0);
+  const countFalse = arr.length - countTrue;
+  const percentageTrue = (countTrue / arr.length) * 100;
+  const percentageFalse = (countFalse / arr.length) * 100;
+  return { true: percentageTrue, false: percentageFalse };
+}
+
 const ComponentWithGyroscope = () => {
   const [alpha, setAlpha] = React.useState(0)
   const [beta, setBeta] = React.useState(0)
   const [gamma, setGamma] = React.useState(0)
+  const [init, setInit] = React.useState(false)
+  const [deflection, setDeflection] = React.useState(false)
+  const [path, setPath] = React.useState<any>([])
+  const [airdrop, setAirdrop] = React.useState(false)
 
-  // const gyroscope = useGyroscope()
   window.addEventListener('deviceorientation', handleOrientation);
+
+  React.useEffect(() => {
+    setDeflection(detectGreaterAngle(alpha, beta, gamma))
+    setPath((prev: any) => [...prev, detectGreaterAngle(alpha, beta, gamma)])
+  }, [alpha, beta, gamma])
 
   function handleOrientation(event: any) {
     const alpha0 = event.alpha;
@@ -23,6 +57,15 @@ const ComponentWithGyroscope = () => {
     setGamma(gamma0)
     // Do stuff...
   }
+
+  React.useEffect(() => {
+    setInterval(() => {
+      const percentages = getPercentageTrueFalse(path)
+      if(percentages.true > .50){
+        setAirdrop(true)
+      }
+    }, 20000)
+  })
 
   function click() {
     console.log(DeviceMotionEvent)
@@ -54,6 +97,8 @@ const ComponentWithGyroscope = () => {
       <li className='prompt'>Y: {beta}</li>
       <li className='prompt'>Z: {gamma}</li>
     </ul>
+    <p>present boogy: {deflection}</p>
+    <p>20s to drop: {airdrop}</p>
     </>
   )
 }
