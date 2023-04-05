cae0f86c5e7e2ab06c8affb77b041f428d736cd0
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-05 18:31:41 -0400

    updated background color change

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
   requestPermission?: () => Promise<'granted' | 'denied'>;
 }
 
+const TIME = 20000
+
 function checkGyroscopeReadings(readings: any, timeframe: number) {
   const movementThreshold = 0.1; // adjust this as needed
   let movementCount = 0;
@@ -46,14 +48,12 @@ const ComponentWithGyroscope = () => {
   const [beta, setBeta] = React.useState(1)
   const [gamma, setGamma] = React.useState(1)
   const [init, setInit] = React.useState(false)
-  const [deflection, setDeflection] = React.useState(false)
   const [path, setPath] = React.useState<any>([])
   const [airdrop, setAirdrop] = React.useState(false)
-  const [percentage, setPercentage] = React.useState(0)
+
   window.addEventListener('deviceorientation', handleOrientation);
 
   React.useEffect(() => {
-    // setDeflection(checkGyroscopeReadings(path, 5000))
     setPath((prev: any) => [...prev, {x: alpha, y: beta, z: gamma}])
   }, [alpha, beta, gamma])
 
@@ -71,27 +71,20 @@ const ComponentWithGyroscope = () => {
     if(!init){
       setInterval((path) => {
         setPath((path: any) => {
-          console.log(path);
-          const check = checkGyroscopeReadings(path, 5000)
+          const check = checkGyroscopeReadings(path, TIME)
           if(check){
             setAirdrop(true)
+            document.getElementsByName('body')[0].style.background = 'cyan'
           }else {
-            alert('experiment failed')
             setAirdrop(false)
+            document.getElementsByName('body')[0].style.background = 'white'
           }
           return path.slice(-500)
         });
 
-      }, 5000, path)
+      }, TIME, path)
       setInit(true)
     }
-
-    // for testing
-    // setInterval(()=> {
-    //   setAlpha(alpha* (1+Math.random()))
-    //   setBeta(beta* (1+Math.random()))
-    //   setGamma(gamma * (1+Math.random()))
-    // },300)
   }, [path, airdrop])
 
   function click() {
@@ -124,10 +117,7 @@ const ComponentWithGyroscope = () => {
       <li className='prompt'>Y: {beta}</li>
       <li className='prompt'>Z: {gamma}</li>
     </ul>
-    <p>present boogy: {deflection.toString()}</p>
     <p>20s to drop: {airdrop.toString()}</p>
-    <p>percentage: {percentage}</p>
-    <p>{path.slice(-10).map((obs: any) => <p>{obs.x}</p>)}</p>
     </>
   )
 }
